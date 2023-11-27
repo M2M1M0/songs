93156f0fa3090ba00025908e1c96b590b34a88f4
m2m1m0 <mikiyassolomonw@gmail.com>
2023-11-27 18:36:03 +0300

    route, middleware config and db connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import cors from "cors"
+import mongoose from "mongoose"
+import songRouter from "./route/song.route.js"
 
+//Init Express
 const app = express()
+
+//MiddleWare
+app.use(express.json())
+app.use(cors())
+
+
+//Routes
+app.use("/song", songRouter)
+
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(8800, () => {
-    console.log("App run on http://localhost:8800")
-})
+
+//Connect to Mongo DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(8800, () => {
+            console.log("App run on http://localhost:8800")
+        })
+    })
+    .catch((err) => console.error("DB connection Error: ", err))
