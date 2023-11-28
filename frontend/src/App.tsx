871d9731f7f0092c85a837f26a63e628388a7798
m2m1m0 <mikiyassolomonw@gmail.com>
2023-11-28 09:19:27 +0300

    frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import AddSong from './pages/AddSong'
+import UpdateSong from './pages/UpdateSong'
+import Sidebar from './components/Sidebar'
+import PlayStation from './components/PlayStation'
 
 function App() {
 
   return (
-    <h1 className="text-xl text-center font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <div className='flex w-full gap-4'>
+        <div className='flex w-1/6'>
+          <Sidebar />
+        </div>
+        <div className='flex w-5/6 border-r-4'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/add-song' element={<AddSong />} />
+            <Route path='/update-song/:id' element={<UpdateSong />} />
+          </Routes>
+        </div>
+      </div >
+    </BrowserRouter>
   )
 }
 
