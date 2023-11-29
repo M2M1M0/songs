eb678ac3d20c155ec090234061ec104625241573
m2m1m0 <mikiyassolomonw@gmail.com>
2023-11-29 20:42:33 +0300

    cloudinary config

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,13 +1,24 @@
 import multer from "multer"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/') // Specify the destination to store the uploaded files
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Multer storage configuration
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Songs', // The folder in your Cloudinary account where the audio files will be stored
+        resource_type: 'auto',
+        allowed_formats: ['mp3', 'wav', "m4a"], // Specify the audio formats you want to allow
     },
-    filename: function (req, file, cb) {
-        cb(null, `audio-${Date.now()}.${file.originalname}`) // Specify how the uploaded files should be named
-    }
-})
-const upload = multer({ storage: storage })
+});
+
+const upload = multer({ storage: storage });
 
 export default upload
