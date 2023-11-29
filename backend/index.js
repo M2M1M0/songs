56c4d008a83b344285d62d26959a348eb1dce7a5
m2m1m0 <mikiyassolomonw@gmail.com>
2023-11-29 23:04:03 +0300

    cloudinary config

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,32 @@ import dotenv from "dotenv"
 dotenv.config()
 import cors from "cors"
 import mongoose from "mongoose"
-import upload from "./middleware/upload.js"
+// import upload from "./middleware/upload.js"
 import songRouter from "./route/song.route.js"
+
+import multer from "multer"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Multer storage configuration
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Songs', // The folder in your Cloudinary account where the audio files will be stored
+        resource_type: 'auto',
+        allowed_formats: ['mp3', 'wav', "m4a"], // Specify the audio formats you want to allow
+    },
+});
+
+const upload = multer({ storage: storage });
+
 
 //Init Express
 const app = express()
